f9fb4bc193b9f3c020afc5797d2c8322cf4b0659
letimberg.cavalcante <letimberg.cavalcante@hemoce.ce.gov.br>
2023-05-31 17:02:22 -0300

    [fullstack] Criando crud

--- app.py
+++ app.py
@@ -11,21 +11,14 @@ db = SQLAlchemy(app)
 class Aluno(db.Model):
     # Configuração do nome e colunas da tabela
     __tablename__ = 'aluno'
-    cpf = db.Column(db.Integer, primary_key=True)
+    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(255))
-    data_nascimento = db.Column(db.String(255))
-    sexo = db.Column(db.String(255))
-    idade = db.Column(db.Integer)
     AV1 = db.Column(db.Float)
     AV2 = db.Column(db.Float)
     media = db.Column(db.Float)
 
-    def __init__(self, cpf, nome, data_nascimento, sexo, idade, AV1, AV2, media):
-        self.cpf = cpf
+    def __init__(self, nome, AV1, AV2, media):
         self.nome = nome
-        self.data_nascimento = data_nascimento
-        self.sexo = sexo
-        self.idade = idade
         self.AV1 = AV1
         self.AV2 = AV2
         self.media = media
@@ -40,6 +33,36 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/cadastrar")
+def cadastrar():
+    return render_template("cadastro.html")
+
+# Cadastrar alunos
+
+
+@app.route("/cadastro", methods=['GET', 'POST'])
+def cadastro():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        av1 = request.form.get("av1")
+        av2 = request.form.get("av2")
+        media = request.form.get("media")
+
+        if nome and av1 and av2 and media:
+            al = Aluno(nome, av1, av2, media)
+            db.session.add(al)
+            db.session.commit()
+    return redirect(url_for("index"))
+
+# Listar alunos
+
+
+@app.route("/lista")
+def lista():
+    alunos = Aluno.query.all()
+    return render_template("lista.html", alunos=alunos)
+
+
 # Inicia o servidor no localhost na porta 8080
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
